c40452307c9c2addb1e429e963b4d60223184846
Hansel Lopez <lopezhansel@gmail.com>
2017-02-25 21:37:08 -0700

    Now passing properties from parent component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <h1>Pixar</h1>
-    <pixar-player></pixar-player>
-    <hello></hello>
+    <pixar-player :sources="video.sources" :options="video.options"></pixar-player>
+    <!--<hello></hello>-->
   </div>
 </template>
 
 <script>
   import Hello from './components/Hello'
-  import pixarPlayer from './components/PixarPlayer'
+  import PixarPlayer from './components/PixarPlayer'
 
   export default {
     name: 'app',
+    data () {
+      return {
+          video: {
+            sources: [
+              {
+                // src: 'http://covteam.u.qiniudn.com/oceans.mp4',
+                src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4',
+                type: 'video/mp4'
+              }
+            ],
+            options: {
+              autoplay: true,
+              volume: 0.6,
+              // poster: 'http://d2zihajmogu5jn.cloudfront.net/tears-of-steel/tears_of_steel.jpg'
+              poster: 'http://covteam.u.qiniudn.com/poster.png'
+            }
+          }
+        }  
+      },
     components: {
       Hello,
-      pixarPlayer 
+      PixarPlayer 
     }
   }
 
--- src/components/PixarPlayer.vue
+++ src/components/PixarPlayer.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="pixar-player">
-    <h1>{{myAwesomeTitle}}</h1>
-    <p>{{renderTime}}</p>
-    <video id="my-video" class="video-js" controls preload="auto" width="640" height="264" poster="http://d2zihajmogu5jn.cloudfront.net/tears-of-steel/tears_of_steel.jpg"
+    <video id="my-video" class="video-js" controls preload="auto" width="640" height="264" :poster="options.poster"
       data-setup="{}">
-      <!-- THESE IS THE STANDARD OF TELLING THE WAY OF TELLING THE PLAYER THE VIDEO INFO-->
-      <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type='video/mp4'>
+      <source v-for="source in sources" :src="source.src" :type="source.type">
+      </source>
       <source src="MY_VIDEO.webm" type='video/webm'>
       <p class="vjs-no-js">
         To view this video please enable JavaScript, and consider upgrading to a web browser that
@@ -18,10 +16,21 @@
 <script>
   export default {
     name: 'pixar-player',
+    props: {
+      sources: Array,
+      options: {
+        type: Object,
+        default () {
+          return {
+            autoplay: false,
+            volume: 0.9,
+            poster: ''
+          }
+        }
+      }
+    },
     data() {
       return {
-        myAwesomeTitle: 'Here is your player , playa',
-        renderTime: new Date().toTimeString(),
       }
     },
     created() {
